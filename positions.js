992e62ee63b7fc812ba4aba83c57f90b0d3d6e75
fevic1 <fevic2@gmail.com>
2026-09-01 21:13:43 -0700

    Empty book state without terminal copy

--- positions.js
+++ positions.js
@@ -1,23 +1,15 @@
 (function () {
   var STORE = "buon_positions_v2";
-
   function empty() {
     var box = document.getElementById("posList");
-    if (box) box.innerHTML = "<div class=\"muted\">No open positions<br><span class=\"meta\">Bot execution paused</span></div>";
+    if (box) box.innerHTML = "<div class=\"pos-empty\"><div class=\"who\">Flat</div><div class=\"meta\">No open book · bot paused</div></div>";
   }
-
   window.clearPositions = function () {
     localStorage.removeItem(STORE);
     localStorage.removeItem("buon_holds_v1");
     empty();
   };
-
   window.recordPosition = function () {};
-
-  window.sellPosition = async function () {
-    empty();
-    return true;
-  };
-
+  window.sellPosition = async function () { empty(); return true; };
   clearPositions();
 })();
